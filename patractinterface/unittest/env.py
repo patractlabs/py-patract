d5cd3ca03a62f4e130441ca283f81a125edd02b6
polkadev <polkadev@protonmail.com>
2021-01-02 17:10:48 +0800

    add env for unittest

---
+++ patractinterface/unittest/env.py
@@ -0,0 +1,58 @@
+import json
+import time
+import logging
+import threading
+from subprocess import *
+from executor import execute_prepared, ExternalCommand
+
+class SubstrateTestEnv:
+    def __init__(self, type_def, port, logLv = logging.DEBUG):
+        self.type_def = type_def
+        self.port = port
+        self.logLv = logLv
+        self.thread = None
+
+        self.name = f'test_{self.type_def}_{self.port}'
+        args = " --name {} --tmp --ws-port {}".format(self.name, self.port)
+        if type_def != 'europa':
+            args += ' --dev'
+        self.cmd = self.type_def + args
+        self.ec = ExternalCommand(self.cmd)
+
+    @classmethod
+    def create_canvas(cls, port=9944):
+        return cls(type_def='canvas', port=port)
+
+    @classmethod
+    def create_europa(cls, port=9944):
+        return cls(type_def='europa', port=port)
+
+    def startNode(self):
+        logging.info("run cmd {}".format(self.cmd))
+
+        def loggerThread():
+            self.ec.start()
+            self.pid = self.ec.pid
+            self.ec.wait()
+
+        self.thread = threading.Thread(target=loggerThread, name=self.name)
+        self.thread.start()
+
+        time.sleep(1) # for startup
+
+        logging.info("startNode {}".format(self.name))
+        return
+
+    def stopNode(self):
+        self.ec.kill()
+        return
+
+    def url(self) -> str:
+        return f'ws://127.0.0.1:{self.port}'
+
+    def typ(self) -> str:
+        if self.type_def == 'canvas':
+            return 'canvas'
+
+        if self.type_def == 'europa':
+            return 'default'
---
+++ test/test_unittest_env.py
@@ -0,0 +1,47 @@
+import os
+import json
+import unittest
+import logging
+
+from scalecodec import ScaleBytes
+from substrateinterface.utils.ss58 import ss58_encode
+from substrateinterface import SubstrateInterface, ContractMetadata, Keypair
+from patractinterface.contracts.erc20 import ERC20
+from patractinterface.unittest.env import SubstrateTestEnv
+
+class UnittestEnvTest(unittest.TestCase):
+    @classmethod
+    def setUp(cls):
+        cls.env = SubstrateTestEnv.create_europa(port=39944)
+        cls.env.startNode()
+        return
+
+    def tearDown(cls):
+        cls.env.stopNode()
+
+    def test_env_canvas(self):
+        # init api
+        substrate=SubstrateInterface(url=self.env.url(), type_registry_preset=self.env.typ())
+
+        alice = Keypair.create_from_uri('//Alice')
+        bob = Keypair.create_from_uri('//Bob')
+
+        # erc20 api
+        erc20 = ERC20.create_from_contracts(
+            substrate= substrate, 
+            contract_file= os.path.join(os.path.dirname(__file__), 'constracts', 'ink', 'erc20.wasm'),
+            metadata_file= os.path.join(os.path.dirname(__file__), 'constracts', 'ink', 'erc20.json')
+        )
+
+        # deplay a erc20 contract
+        erc20.putAndDeploy(alice, 1000000 * (10 ** 15))
+
+        erc20.transferFrom(alice,
+            fromAcc=alice.ss58_address, 
+            toAcc=bob.ss58_address, 
+            amt=10000)
+
+        erc20.transfer(alice, bob.ss58_address, 10000)
+
+if __name__ == '__main__':
+    unittest.main()

37c10ce41ec2cd814159f577c601bb81a25294c5
99Kies <1290017556@qq.com>
2021-11-30 22:12:59 +0800

    fixed test_base

--- test/test_base.py
+++ test/test_base.py
@@ -51,9 +51,9 @@ class ContractSubscriberTestCase(unittest.TestCase):
             substrate=self.substrate
         )
 
-        typ15 = contract_metadata.get_type_string_for_metadata_type(15)
-        logging.debug("typ15 {}".format(typ15))
-        decoder = ScaleDecoder.get_decoder_class(typ15, 
+        typ14 = contract_metadata.get_type_string_for_metadata_type(14)
+        logging.debug("typ15 {}".format(typ14))
+        decoder = ScaleDecoder.get_decoder_class(typ14, 
             ScaleBytes("0x01d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d"),
             self.substrate.runtime_config)
         evtTransferArgs = decoder.decode()
